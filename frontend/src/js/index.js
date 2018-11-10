e69a9e6e77c41078bff59649f98607218d0b28b9
Daniel <danielmtesfai@gmail.com>
2018-11-10 01:11:32 -0500

    now displaying some data on UI

--- frontend/src/js/index.js
+++ frontend/src/js/index.js
@@ -1,11 +1,27 @@
+function getJson(callback){
+    let city = $('#city').val();
+    $.ajax({
+        type: "GET",
+        url: "/api/city/" + city,
+        success: callback
+    })
+}
+
+results = {"Snow": "It's snowing!", "Rain": "It's raining.", "Clouds": "It's cloudy."}
+
 $(document).ready(function () {
+    // $("#results").hide();
     $("#city").keyup(
         function (e) {
         	if (e.keyCode == 13){
-			    let city = $('#city').val();
-                $.ajax({
-                	type: "GET",
-                	url: "/api/city/" + city
+                getJson(function(response){
+                    console.log(response);
+                    $("#search").hide();
+                    $("#name").append(response['name'] + " " + response['country']);
+                    $("#sunrise").append(response['sunrise']);
+                    $("#sunset").append(response['sunset']);
+                    $("#conditions").append(results[response['condit']]);
+                    $("#results").show();
                 })
         	}
         }
